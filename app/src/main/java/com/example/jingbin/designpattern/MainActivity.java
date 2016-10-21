50e7efaa2ab079aab70f49ca4f7db1fd2c525cdb
youlookwhat <770413277@qq.com>
2016-10-21 19:06:06 +0800

    first commit

---
+++ app/src/main/java/com/example/jingbin/designpattern/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jingbin.designpattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
