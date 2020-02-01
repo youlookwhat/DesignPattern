cfa917cd3d15591634b5fdc47a2c7734a164aeb0
youlookwhat <770413277@qq.com>
2020-02-01 15:50:59 +0800

    change ic_launcher

---
+++ app/src/main/java/com/example/jingbin/designpattern/proxy/ProxyActivity.java
@@ -0,0 +1,18 @@
+package com.example.jingbin.designpattern.proxy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jingbin.designpattern.R;
+
+/**
+ * @author jingbin
+ */
+public class ProxyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proxy);
+    }
+}
